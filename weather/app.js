cc66849f2459f4a7d4e74d2260ecd3b8fc37d11f
Anujverma2323 <vermanuj2323@gmail.com>
2025-01-25 14:58:38 +0530

    Fix wrong program namee

--- weather/app.js
+++ weather/app.js
@@ -18,7 +18,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
 app.use('/', indexRouter);
+
+var app = express();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
